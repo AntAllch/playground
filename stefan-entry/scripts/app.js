a5ea43cd8b45c1aebdc275df443543ca5df4e93e
Stefan Allchorne <allchorne39@sky.com>
2025-03-24 19:47:07 +0000

    Add animations

--- stefan-entry/scripts/app.js
+++ stefan-entry/scripts/app.js
@@ -1,5 +1,7 @@
 const card = document.querySelector('.card');
+const reference = document.querySelector('.reference');
 const shimmer = document.querySelector('.shimmer');
+const animate = document.querySelectorAll('.animate');
 
 const rotate = document.querySelector('.rotate');
 const halfRotation = document.querySelector('.half-rotation');
@@ -9,7 +11,19 @@ const firstEditionIcon = document.querySelector('.pokemon-attributes img');
 
 const buttons = document.querySelectorAll('button');
 
-document.addEventListener('DOMContentLoaded', () => {    
+document.addEventListener('DOMContentLoaded', () => {   
+    
+    document.documentElement.classList.add('show');
+    
+    setTimeout(() => {
+        animate.forEach((item, index) => {
+            setTimeout(() => {
+                item.classList.add('revealed');
+                console.log(item);
+            }, index * 200)
+        })
+    }, 500)
+
     rotate.addEventListener('click', () => {
         card.classList.toggle('rotated');
         shimmer.classList.toggle('rotated');
